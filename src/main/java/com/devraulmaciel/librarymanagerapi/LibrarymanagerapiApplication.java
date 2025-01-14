e239367e4cb9b107d2f9eb077c89842252ac4e11
raulmaciel0 <raul.maciel015@gmail.com>
2025-01-14 19:13:42 -0300

    Primeiro Commit - Estrutura inicial do projeto Resolvendo conflito no .gitignore

---
+++ src/main/java/com/devraulmaciel/librarymanagerapi/LibrarymanagerapiApplication.java
@@ -0,0 +1,13 @@
+package com.devraulmaciel.librarymanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devraulmaciel/librarymanagerapi/LibrarymanagerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devraulmaciel.librarymanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
